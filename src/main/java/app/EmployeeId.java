61f907dbf4b5c5215160736886e92c9b51f228ca
Kohei Nozaki <kohei@apache.org>
2016-10-29 12:36:25 +0900

    must be larger than zero, not one

--- src/main/java/app/EmployeeId.java
+++ src/main/java/app/EmployeeId.java
@@ -5,7 +5,7 @@ public class EmployeeId {
     private final long value;
 
     public EmployeeId(final long value) {
-        if (value < 1) {
+        if (value < 0) {
             throw new IllegalArgumentException("EmployeeId must be larger than zero");
         }
 
