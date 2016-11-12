4eda2c140fd620914e158a6acdef91819675c313
Kohei Nozaki <kohei@apache.org>
2016-11-12 11:32:02 +0900

    rename the name of the mapper

--- src/main/java/app/ConstraintViolationExceptionMapper.java
+++ src/main/java/app/ConstraintViolationExceptionMapper.java
@@ -6,7 +6,7 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
 
     @Override
     public Response toResponse(final ConstraintViolationException e) {
